87cdca7bc87b51211d259d8e211d1b020c15a13f
Paulo Leal <paulo.leal@a4pm.com.br>
2023-02-15 11:16:21 -0300

    Finalizado!

--- script/main.js
+++ script/main.js
@@ -1,12 +1,63 @@
+const pokemonName = document.querySelector(".pokemon-name");
+const pokemonNumber = document.querySelector(".pokemon-number");
+const pokemonImage = document.querySelector(".pokemon-image");
+const input = document.querySelector(".input-search");
+
+let searchPokemon = 94;
+
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+
+const form = document.querySelector(".form");
+
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   );
-
-  const data = await APIResponse.json();
-  return data;
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
 };
 
-const renderPokemon = (pokemon) => {
-  const data = fetchPokemon(pokemon);
+const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = "Loading...";
+  pokemonNumber.innerHTML = "";
+
+  const data = await fetchPokemon(pokemon);
+  if (data) {
+    pokemonImage.style.display = "block";
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src =
+      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+        "front_default"
+      ];
+
+    input.value = "";
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = "none";
+    pokemonName.innerHTML = "Not found :(";
+    pokemonNumber.innerHTML = "";
+  }
 };
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+btnPrev.addEventListener("click", () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+btnNext.addEventListener("click", () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
